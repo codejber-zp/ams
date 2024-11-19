219e4f9ed6b6c551c844de6d882a48a8df42efee
Ziemowit Przebitkowski <codejber@ziemowitprzebitkowski.pl>
2024-11-19 21:42:09 +0100

    fix(ci): use docker container to serve static

--- apps/lms/next.config.js
+++ apps/lms/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -12,6 +11,7 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  output: 'standalone',
 };
 
 const plugins = [
@@ -19,4 +19,4 @@ const plugins = [
   withNx,
 ];
 
-module.exports = { ...composePlugins(...plugins)(nextConfig), output: 'standalone'};
+module.exports = composePlugins(...plugins)(nextConfig);
